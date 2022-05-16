d028725eb9d676bcd81cce68d715f5f011bb1723
Daichi Tanaka <tanakadaichi_1989@icloud.com>
2022-05-16 19:40:52 +0900

    Initial Commit

---
+++ WalkMeter/WalkMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkMeterApp.swift
+//  WalkMeter
+//
+//  Created by 田中大地 on 2022/05/16.
+//
+
+import SwiftUI
+
+@main
+struct WalkMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
