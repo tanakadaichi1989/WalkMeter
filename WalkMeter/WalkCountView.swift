8dd8f5cc0ad9fbb166377c01764b7d507cc82f40
Daichi Tanaka <tanakadaichi_1989@icloud.com>
2022-05-21 22:00:04 +0900

    Add WalkCountView

---
+++ WalkMeter/WalkCountView.swift
@@ -0,0 +1,32 @@
+//
+//  WalkCountView.swift
+//  WalkMeter
+//
+//  Created by 田中大地 on 2022/05/21.
+//
+
+import SwiftUI
+
+struct WalkCountView: View {
+    var label: String
+    var walkCount: Int
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(label)
+                .font(.subheadline)
+            Text(walkCount.description)
+                .fontWeight(.heavy)
+        }
+        .frame(height: 100)
+        .padding()
+        .border(.bar, width: 2)
+        .cornerRadius(10)
+    }
+}
+
+struct WalkCountView_Previews: PreviewProvider {
+    static var previews: some View {
+        WalkCountView(label: "今日の歩数", walkCount: 10000)
+    }
+}
